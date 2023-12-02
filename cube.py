4c5c5dfe0c171d9574fee35a7873a23e77d08507
Greg Kronmiller <kronmillerg@gmail.com>
2023-12-02 15:52:23 -0800

    Add code to print the cube

--- cube.py
+++ cube.py
@@ -1,18 +1,61 @@
 import numpy as np
 
+C_W = 0
+C_O = 1
+C_G = 2
+C_R = 3
+C_B = 4
+C_Y = 5
+
+_colorToLetter = {
+    C_W: 'W',
+    C_O: 'O',
+    C_G: 'G',
+    C_R: 'R',
+    C_B: 'B',
+    C_Y: 'Y',
+}
 
 class Cube:
     def __init__(self):
-        self.u = np.ones((3, 3), dtype=int) * 0
-        self.l = np.ones((3, 3), dtype=int) * 1
-        self.f = np.ones((3, 3), dtype=int) * 2
-        self.r = np.ones((3, 3), dtype=int) * 3
-        self.b = np.ones((3, 3), dtype=int) * 4
-        self.d = np.ones((3, 3), dtype=int) * 5
+        self.u = np.ones((3, 3), dtype=int) * C_W
+        self.l = np.ones((3, 3), dtype=int) * C_O
+        self.f = np.ones((3, 3), dtype=int) * C_G
+        self.r = np.ones((3, 3), dtype=int) * C_R
+        self.b = np.ones((3, 3), dtype=int) * C_B
+        self.d = np.ones((3, 3), dtype=int) * C_Y
 
     def __repr__(self):
-        # FIXME: Write this.
-        pass
+        # FIXME: Do this without 6*3*3 local variables.
+        [[utl, utm, utr], [uml, umm, umr], [ubl, ubm, ubr]] = cube.u
+        [[ltl, ltm, ltr], [lml, lmm, lmr], [lbl, lbm, lbr]] = cube.l
+        [[ftl, ftm, ftr], [fml, fmm, fmr], [fbl, fbm, fbr]] = cube.f
+        [[rtl, rtm, rtr], [rml, rmm, rmr], [rbl, rbm, rbr]] = cube.r
+        [[btl, btm, btr], [bml, bmm, bmr], [bbl, bbm, bbr]] = cube.b
+        [[dtl, dtm, dtr], [dml, dmm, dmr], [dbl, dbm, dbr]] = cube.d
+        return """\
+        {} {} {}
+        {} {} {}
+        {} {} {}
+
+{} {} {}   {} {} {}   {} {} {}   {} {} {}
+{} {} {}   {} {} {}   {} {} {}   {} {} {}
+{} {} {}   {} {} {}   {} {} {}   {} {} {}
+
+        {} {} {}
+        {} {} {}
+        {} {} {}""".format(*[_colorToLetter[x] for x in [
+                             utl, utm, utr,
+                             uml, umm, umr,
+                             ubl, ubm, ubr,
+
+            ltl, ltm, ltr,   ftl, ftm, ftr,   rtl, rtm, rtr,   btl, btm, btr,
+            lml, lmm, lmr,   fml, fmm, fmr,   rml, rmm, rmr,   bml, bmm, bmr,
+            lbl, lbm, lbr,   fbl, fbm, fbr,   rbl, rbm, rbr,   bbl, bbm, bbr,
+
+                             dtl, dtm, dtr,
+                             dml, dmm, dmr,
+                             dbl, dbm, dbr]])
 
     def rotateU(self):
         """ Rotate U face 90 degrees clockwise. """
@@ -33,4 +76,9 @@ class Cube:
 
 def _cyclicallyRotate(a, b, c, d):
     a[:], b[:], c[:], d[:] = d, a, b, c
+
+
+if __name__ == '__main__':
+    cube = Cube()
+    print(cube)
 
