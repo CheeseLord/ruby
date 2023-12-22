5fb883bbf29684c527a94142c2ea6e1aac9f5ace
Greg Kronmiller <kronmillerg@gmail.com>
2023-12-22 01:57:08 -0800

    Implement parsing of cube algorithms

--- cube_cubies_take2.py
+++ cube_cubies_take2.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+import re  # Now we have two problems
 
 
 ########################################
@@ -11,8 +12,12 @@ def main():
     selfTest()
     cube = Cube()
     print(cube)
-    print("\n" + "-"*29 + "\n")
+    print('\n' + '-'*29 + '\n')
     cube.rotateFace(F_R)
+    print(cube)
+    print('\n' + '-'*29 + '\n')
+    # T perm (minus the initial R that we already did)
+    cube.doMoves("U R' U' R' F R2 U' R' U' R U R' F'")
     print(cube)
 
 def selfTest():
@@ -39,6 +44,15 @@ F_F = 2
 F_R = 3
 F_B = 4
 F_D = 5
+
+_oppositeFace = {
+    F_R: F_L,
+    F_L: F_R,
+    F_U: F_D,
+    F_D: F_U,
+    F_F: F_B,
+    F_B: F_F,
+}
 
 _faceToLetter = {
     F_U: 'U',
@@ -68,13 +82,140 @@ _axesForClockwiseRotation = {
     F_B : (1, 2), # down  to right
 }
 
+# TODO compute _faceToLetter automatically from this
+_notationFaceTurns = {
+    'R': F_R,
+    'L': F_L,
+    'U': F_U,
+    'D': F_D,
+    'F': F_F,
+    'B': F_B,
+}
+
+_notationWholeCube = {
+    'x': F_R,
+    'y': F_U,
+    'z': F_F,
+}
+
+_notationMiddleSlice = {
+    'M': F_L, # NOTE! Opposite direction from 'x'
+    'E': F_D, # NOTE! Opposite direction from 'y'
+    'S': F_F, # Same direction as 'z'
+}
+
 def frontSlice(cubies):
     return cubies[2]
+
+# When parsing a cube algorithm like "R U R'", primarily split on whitespace.
+# However, sometimes for longer algorithms, people also add parentheses to show
+# portions of the algorithm that are easier to remember together, as in this
+# version of the Ra perm:
+#     (R U' R' U') (R U R D) (R' U' R D') (R' U2' R')
+# When parsing an algorithm, don't enforce that the parentheses group sensibly;
+# just split on any whitespace or parentheses.
+#
+# Note that we have to use non-capturing parentheses here, else the matched
+# groups would be included in the output of re.split(). See:
+#     https://stackoverflow.com/a/10974950
+_algSplitRe = re.compile(r'(?:\s|\(|\))+')
 
 class Cube:
     def __init__(self):
         self.cubies = np.full((3, 3, 3), CANONICAL_CUBIE)
 
+    def doMoves(self, moves):
+        # If moves is given as a string, split it using _algSplitRe before we
+        # start parsing it. Note that this means you have to put spaces between
+        # all the moves in your algorithms; you can't just write "RUR'U'" and
+        # expect the Cube class to sort it out. For a 3x3 cube it would be
+        # possible to parse the moves without this pre-splitting logic:
+        # [RLUDFBrludfbxyzMES] always introduce a new move, [w2'] never do, and
+        # no other characters are used. But for larger cubes, there is an
+        # ambiguity in the notation, so it would be bad precedent to allow
+        # this. For wide and slice moves on larger cubes, the number of layers
+        # or the slice is specified by prepending a number to the move: e.g.,
+        # 3Rw turns the rightmost 3 layers, and 3R turns the third layer from
+        # the right. So the string "R2U" could mean either "R2 U" or "R 2U".
+        if type(moves) == str:
+            moves = _algSplitRe.split(moves)
+        for move in moves:
+            logging.debug("\n\n>>> EXECUTE MOVE: %s", move)
+            # Skip empty strings in case moves started/ended with a paren.
+            if move:
+                self.doOneMove(move)
+            logging.debug('After executing move: %s\n%s', move, self)
+
+    def doOneMove(self, move):
+        face   = None
+        isWide = False
+
+        faceLetter = move[0]
+        rest       = move[1:]
+
+        # '[RLUDFB]' indicate a normal face turn
+        face = _notationFaceTurns.get(faceLetter, None)
+        if face is not None:
+            # '[RLUDFB]w' indicate a wide face turn (turn 2 layers at once)
+            if rest and rest[0] == 'w':
+                isWide = True
+                rest = rest[1:]
+        # '[rludfb]' also indicate a wide face turn
+        elif faceLetter.islower():
+            face = _notationFaceTurns.get(faceLetter.upper(), None)
+
+        numTurns = 1
+        # Note: intentionally allow "2'" since I've seen that used to indicate
+        # "rotate twice counterclockwise" (it's functionally equivalent to just
+        # "2", but provides a hint about which way you actually turn the face
+        # when executing). Probably we should allow "'2" similarly, though I
+        # haven't seen it yet.
+        if rest and rest[0] == '2':
+            numTurns = 2
+            rest = rest[1:]
+        if rest and rest[0] == "'":
+            numTurns = -numTurns
+            rest = rest[1:]
+        if rest:
+            raise ValueError(f'Unexpected characters at end of move: {move!r}')
+
+        # Handle normal face turns first because they're the common case
+        if face is not None:
+            if not isWide:
+                self.rotateFace(face, numTurns)
+            else:
+                # For a wide turn, rotate the whole cube and then rotate the
+                # opposite face back. Note that because the turns are clockwise
+                # relative to the face being turned, we do not need to negate
+                # numTurns when rotating the opposite face. For example, Rw is
+                # equivalent to x L, NOT x L'.
+                self.rotateWholeCube(face, numTurns)
+                self.rotateFace(_oppositeFace[face], numTurns)
+            return
+
+        # Not a normal face turn. Either a whole-cube rotation or a middle
+        # slice turn (or an error).
+        face = _notationWholeCube.get(faceLetter, None)
+        if face is not None:
+            self.rotateWholeCube(face, numTurns)
+            return
+
+        face = _notationMiddleSlice.get(faceLetter, None)
+        if face is not None:
+            # For a middle-slice turn, do a wide turn of the corresponding face
+            # in the same direction, then a normal turn in the opposite
+            # direction. For example, M is equivalent to Lw L'.
+            # Wide turn
+            self.rotateWholeCube(face, numTurns)
+            self.rotateFace(_oppositeFace[face], numTurns)
+            # Normal turn in opposite direction
+            self.rotateFace(face, -numTurns)
+            return
+
+        raise ValueError(f'Unrecognized face letter in move: {move!r}')
+
+    # TODO rename numQuarterTurns to numTurns, or maybe just turns, in a few
+    # functions? Maybe also aboutFace -> face.
     def rotateFace(self, aboutFace, numQuarterTurns=1):
         '''
         Rotate just one face of the cube.
@@ -91,9 +232,11 @@ class Cube:
 
         self._debugCube("=> rotateFace after rotate to front")
 
-        # Next, rotate the front face by the requested amount.
+        # Next, rotate the front face by the requested amount. Note that rot90
+        # defaults to the axes in counterclockwise order, so negate
+        # numQuarterTurns for a clockwise rotation.
         # TODO factor out 2, as with frontSlice.
-        self.cubies[2] = np.rot90(self.cubies[2], k=numQuarterTurns)
+        self.cubies[2] = np.rot90(self.cubies[2], k=-numQuarterTurns)
         self.cubies[2] = self._rotateCubies(self.cubies[2], F_F,
                 numQuarterTurns)
 
@@ -271,14 +414,14 @@ CANONICAL_CUBIE = makeCubie(C_G, C_Y)
 def rotateCubie(cubie, face, numQuarterTurns=1):
     fdr = list(getCubieFDR(cubie))
     rotAxes = _axesForClockwiseRotation[face]
-    logging.debug("rotateCubie(%s, %s, %d)" %
-            (_debugCubieStr(cubie), _faceToLetter[face], numQuarterTurns))
+    #logging.debug("rotateCubie(%s, %s, %d)" %
+    #        (_debugCubieStr(cubie), _faceToLetter[face], numQuarterTurns))
     for i in range(numQuarterTurns % 4):
         oldFdr = fdr[:]
         fdr[rotAxes[1]] = oldFdr[rotAxes[0]]
         fdr[rotAxes[0]] = oppositeColor(oldFdr[rotAxes[1]])
-        logging.debug("=> After %d rotations: %s" %
-                (i + 1, "".join([_colorToLetter[c] for c in fdr])))
+        #logging.debug("=> After %d rotations: %s" %
+        #        (i + 1, "".join([_colorToLetter[c] for c in fdr])))
     front, down, _ = fdr
     return makeCubie(front, down)
 
